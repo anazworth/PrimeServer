0551d1dcb2658211391a0dc274f14d6fb112d8a1
anazworth <98629900+anazworth@users.noreply.github.com>
2023-04-20 18:48:14 -0400

    Jar complete

---
+++ src/main/java/com/anazworth/primeserver/PrimeController.java
@@ -0,0 +1,26 @@
+package com.anazworth.primeserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeController {
+
+    @GetMapping("/primes/{number}")
+    public Boolean getPrimes(@PathVariable Integer number) {
+        return isPrime(number);
+    }
+
+    private Boolean isPrime(Integer number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
